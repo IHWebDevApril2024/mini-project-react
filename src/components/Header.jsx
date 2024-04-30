0b725b282fc4770f9ef8940eb49e7b1819e49aa2
Marshall-Bits <kz_@hotmail.es>
2024-04-30 17:04:42 +0200

    I'm not super smart after 17

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Website is awesome and I'm working on main branch without communicating it to my team members</h1>
+    </header>
+  );
+}
